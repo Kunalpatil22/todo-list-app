8a8ac0d7e46adde67bf82953fbea312819d753cf
Kunalpatil22 <kunalpatil2203@gmail.com>
2023-09-10 17:35:52 +0530

    index.js: static middleware setup for serving production build of React Application

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const config = require('./config');
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const config = require("./config");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const todosRoutes = require('./routes/todos.routes');
+const todosRoutes = require("./routes/todos.routes");
 
 const { port, mongodb_uri } = config;
 
 const app = express();
 
-mongoose.connect(mongodb_uri).then(() => {
+mongoose
+  .connect(mongodb_uri)
+  .then(() => {
     console.log("Database connection established.");
-}).catch((err) => {
-    console.error('Unable to connect database: ', err);
+  })
+  .catch((err) => {
+    console.error("Unable to connect database: ", err);
     process.exit(1);
-});
+  });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static("./client/build"));
 
-app.use('/api/todos', todosRoutes);
+app.use("/api/todos", todosRoutes);
 
 app.listen(port, () => {
-    console.log(`Server started.`);
-})
+  console.log(`Server started.`);
+});
